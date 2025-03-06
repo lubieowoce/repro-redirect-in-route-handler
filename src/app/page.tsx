5d18973de63ca2dee868126e6b3fc1464cee24b9
Janka Uryga <lolzatu2@gmail.com>
2025-03-06 04:15:15 +0100

    add test page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,113 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+/* eslint-disable react/no-unescaped-entities */
+"use client";
+
+import { type ReactNode, useState } from "react";
 
 export default function Home() {
   return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <main style={{ maxWidth: "100ch" }}>
+      <h1>
+        What's the result of POSTing to a route handler <br />
+        that redirect()s to a static page?
+      </h1>
+      {process.env.NODE_ENV === "development" && (
+        <p style={{ border: "1px solid lightgrey", padding: "1em" }}>
+          <strong>WARNING</strong>: this will not work correctly in dev mode,
+          because we're not returning 405 for POST requests to static pages
+        </p>
+      )}
+      <section>
+        <h2>❌ 405</h2>
+        <p>(because it returns a 307, and the POST is retried)</p>
+        <SendRequest
+          action={() =>
+            fetch("/redirecting-handler", {
+              method: "POST",
+              headers: { "content-type": "application/json" },
+              body: JSON.stringify({ what: "ever" }),
+            })
+          }
+        >
+          POST json
+        </SendRequest>
+        <div style={{ height: "0.5em" }}></div>
+        <SendRequest
+          action={() =>
+            fetch("/redirecting-handler", {
+              method: "POST",
+              headers: { "content-type": "text/plain" },
+              body: "whatever",
+            })
+          }
+        >
+          POST plaintext
+        </SendRequest>
+      </section>
+      <br />
+      <section>
+        <h2>✅ 200</h2>
+        <p>(because it returns a 303, and the browser does a GET instead)</p>
+        <SendRequest
+          action={() =>
+            fetch("/redirecting-handler", {
+              method: "POST",
+              body: (() => {
+                const formData = new FormData();
+                formData.append("what", "ever");
+                return formData;
+              })(),
+            })
+          }
+        >
+          POST multipart
+        </SendRequest>
+        <div style={{ height: "0.5em" }}></div>
+        <SendRequest
+          action={() =>
+            fetch("/redirecting-handler", {
+              method: "POST",
+              body: new URLSearchParams({ what: "ever" }),
+            })
+          }
+        >
+          POST urlencoded
+        </SendRequest>
+      </section>
+    </main>
+  );
+}
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+function SendRequest({
+  action,
+  children,
+}: {
+  action: () => Promise<Response>;
+  children: ReactNode;
+}) {
+  const [result, setResult] = useState<NonNullable<ReactNode> | null>(null);
+
+  return (
+    <div style={{ display: "flex", alignItems: "center", gap: "1em" }}>
+      <button
+        type="button"
+        style={{
+          minWidth: "18ch",
+          fontFamily: "monospace",
+          textAlign: "start",
+        }}
+        onClick={() =>
+          action().then((res) =>
+            setResult(
+              <span style={{ color: res.ok ? "green" : "tomato" }}>
+                response status: {res.status}
+              </span>
+            )
+          )
+        }
+      >
+        {children}
+      </button>
+      {result !== null && <div>{result}</div>}
     </div>
   );
 }
